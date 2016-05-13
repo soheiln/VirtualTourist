ca3957bc14cd96096804a6268200c23f93fa8e55
Soheil Norouzi <soheil.nrz@gmail.com>
2016-05-13 10:40:58 -0700

    Add initial views and CoreDataStackManager

---
+++ VirtualTourist/VirtualTourist/PhotosViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PhotosViewController.swift
+//  VirtualTourist
+//
+//  Created by soheiln on 5/11/16.
+//  Copyright © 2016 soheiln. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+}
